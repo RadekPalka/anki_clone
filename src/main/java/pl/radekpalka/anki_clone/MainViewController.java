4d0de138a94ac35b77d2ad4849a25e97b8b8c2c3
Radosław Pałka <radekpalka@gmail.com>
2025-04-02 18:46:56 +0200

    feat: create main scene

---
+++ src/main/java/pl/radekpalka/anki_clone/MainViewController.java
@@ -0,0 +1,13 @@
+package pl.radekpalka.anki_clone;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class MainViewController {
+
+    @FXML
+    private void handleClick() throws IOException {
+        System.out.println("Hello");
+    }
+}
