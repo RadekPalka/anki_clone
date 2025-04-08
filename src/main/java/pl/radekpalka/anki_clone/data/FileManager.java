97231ed2f364af146670da519374647fce2fcfc3
Radosław Pałka <radekpalka@gmail.com>
2025-04-08 17:54:08 +0200

    feat: create loadAllDecks method

--- src/main/java/pl/radekpalka/anki_clone/data/FileManager.java
+++ src/main/java/pl/radekpalka/anki_clone/data/FileManager.java
@@ -1,8 +1,11 @@
 package pl.radekpalka.anki_clone.data;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -28,4 +31,25 @@ public class FileManager {
             e.printStackTrace();
         }
     }
+
+    public static List<Deck> loadAllDecks(String directoryPath) {
+    File dir = new File(directoryPath);
+    if (!dir.exists()) return List.of();
+
+    File[] files = dir.listFiles((d, name) -> name.endsWith(".json"));
+    if (files == null) return List.of();
+
+    List<Deck> decks = new ArrayList<>();
+    for (File file : files) {
+        try (FileReader reader = new FileReader(file)) {
+            Deck deck = gson.fromJson(reader, Deck.class);
+            decks.add(deck);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    return decks;
+}
+
 }
