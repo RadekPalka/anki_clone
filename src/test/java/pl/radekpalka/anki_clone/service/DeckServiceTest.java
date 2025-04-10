e7adf7ec96cf308707ec6462bec6bcfb3a84bc6b
Radosław Pałka <radekpalka@gmail.com>
2025-04-10 19:12:53 +0200

    test: prevent adding flashcard with empty or null front/back

--- src/test/java/pl/radekpalka/anki_clone/service/DeckServiceTest.java
+++ src/test/java/pl/radekpalka/anki_clone/service/DeckServiceTest.java
@@ -1,14 +1,42 @@
 package pl.radekpalka.anki_clone.service;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pl.radekpalka.anki_clone.model.Deck;
+
 public class DeckServiceTest {
+
+    private Deck deck;
+
+    @BeforeEach
+    void setup(){
+        deck = new Deck("Testing");
+    }
     
     @Test
-    void twoPlusTwoShouldEqualFour() {
-        int result = 2 + 2;
-        assertEquals(4, result);
+    void flashcardWithTwoEmptyStringsIsNotAdded() {
+        DeckService.addFlashcardIfValid(deck, "", "");
+        assertTrue(deck.getFlashcards().isEmpty());
+    }
+
+    @Test
+    void flashCardWithOneEmptyStringIsNotAdded(){
+        DeckService.addFlashcardIfValid(deck, "front", "");
+        assertTrue(deck.getFlashcards().isEmpty());
+    }
+
+    @Test
+    void flashCardWithTwoNullsIsNotAdded(){
+        DeckService.addFlashcardIfValid(deck, null, null);
+        assertTrue(deck.getFlashcards().isEmpty());
+    }
+
+    @Test
+    void flashCardWithOneNullIsNotAdded(){
+        DeckService.addFlashcardIfValid(deck, "front", null);
+        assertTrue(deck.getFlashcards().isEmpty());
     }
 }
