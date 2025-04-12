b51f62c86c7f5e9771a5ce6b598966015d240fa4
Radosław Pałka <radekpalka@gmail.com>
2025-04-12 09:58:22 +0200

    fix: ignore flashcards with only whitespace input

--- src/main/java/pl/radekpalka/anki_clone/service/DeckService.java
+++ src/main/java/pl/radekpalka/anki_clone/service/DeckService.java
@@ -5,6 +5,11 @@ import pl.radekpalka.anki_clone.model.Flashcard;
 
 public class DeckService {
     public static void addFlashcardIfValid(Deck deck, String front, String back){
+        if (front == null || back == null) return;
+
+        front = front.trim();
+        back = back.trim();
+        
         if (front != null && back != null && !front.isEmpty() && !back.isEmpty()){
                 var flashcard = new Flashcard(front, back);
                 deck.addFlashcard(flashcard);
