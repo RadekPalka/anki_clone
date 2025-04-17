d9f166df22c9b78dbe81984645c61a4e687b36d0
Radosław Pałka <radekpalka@gmail.com>
2025-04-17 18:46:44 +0200

    test: ensure flashcards are correctly persisted and restored from file

--- src/test/java/pl/radekpalka/anki_clone/integration/FileManagerIntegrationTest.java
+++ src/test/java/pl/radekpalka/anki_clone/integration/FileManagerIntegrationTest.java
@@ -9,20 +9,21 @@ import org.junit.jupiter.api.Test;
 
 import pl.radekpalka.anki_clone.data.FileManager;
 import pl.radekpalka.anki_clone.model.Deck;
+import pl.radekpalka.anki_clone.model.Flashcard;
 
 public class FileManagerIntegrationTest {
+    static final String TEST_FILE_PATH = "testing-decks";
     @Test
     void shouldSaveAndLoadDeckCorrectly(){
-        String testFilePath = "testing-decks";
         Deck originalDeck = new Deck("Integration deck test");
 
-        FileManager.saveDeck(originalDeck, testFilePath);
-        List<Deck> loadedDecks = FileManager.loadAllDecks(testFilePath);
+        FileManager.saveDeck(originalDeck, TEST_FILE_PATH);
+        List<Deck> loadedDecks = FileManager.loadAllDecks(TEST_FILE_PATH);
 
         assertFalse(loadedDecks.isEmpty(), "Deck list should not be empty");
         assertEquals(originalDeck.getTitle(), loadedDecks.get(0).getTitle(), "Deck title should match");
         
-        File testFolder = new File(testFilePath);
+        File testFolder = new File(TEST_FILE_PATH);
         File[] files = testFolder.listFiles();
         if (files != null) {
             for (File file : files) {
@@ -30,6 +31,24 @@ public class FileManagerIntegrationTest {
             }
         }
         testFolder.delete();
+    }
+
+    @Test
+    void shouldSaveAndLoadDeckWithFlashcardsCorrectly(){
+        Deck originalDeck = new Deck("Testing");
+        String front = "test front";
+        String back = "test back";
+        originalDeck.getFlashcards().add(new Flashcard(front, back));
+
+        FileManager.saveDeck(originalDeck, TEST_FILE_PATH);
+        List<Deck> loadedDecks = FileManager.loadAllDecks(TEST_FILE_PATH);
+        List<Flashcard> testFlashcards = loadedDecks.get(0).getFlashcards();
+
+        assertEquals(originalDeck.getFlashcards().size(), testFlashcards.size(), "Size should be one");
+
+        assertEquals(front, testFlashcards.get(0).getFront(), "Front should be 'test front");
+
+        assertEquals(back, testFlashcards.get(0).getBack(), "Front should be 'test back");
     }
 
 }
